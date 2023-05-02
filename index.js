e197ce354f85581a31b716cd6ecb0ddbd63f68f1
Alina Gattermann <gattermannalina@gmail.com>
2023-05-02 16:28:06 +0200

    onclick alert

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const recipeList = document.querySelector('#recipe-list');
 const recipeIframe = document.querySelector('#recipe-iframe');
+ecipe = document.getElementById('#recipe')
 
 recipeList.addEventListener('change', function () {
     const selectedRecipe = recipeList.value;
     recipeIframe.src = selectedRecipe;
 });
+
+function shared() {
+    alert("Shared sucessfully");
+}
+
